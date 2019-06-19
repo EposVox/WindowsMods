1f401df5acc4cda103d2d63700dd051665370d7d
Adam Taylor <eposvox@gmail.com>
2019-06-19 00:17:57 -0400

    Backup script

---
+++ backups.bat
@@ -0,0 +1,4 @@
+xcopy /D /E /I /Q /-Y "E:\Pictures" "K:\Pictures"
+xcopy /D /E /I /Q /-Y "E:\Pictures" "X:\Adam Backup\Pictures"
+xcopy /D /E /I /Q /-Y "E:\Documents" "X:\Adam Backup\Documents"
+xcopy /D /E /I /Q /-Y "E:\Music" "X:\Adam Backup\Music1"
