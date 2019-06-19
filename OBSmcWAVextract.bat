ca233079cb84fd5a7c869785e798987491566d08
Adam Taylor <eposvox@gmail.com>
2019-06-19 00:16:59 -0400

    Extract WAV files from multi-track audio recording
    doesn't work?

---
+++ OBSmcWAVextract.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set /a count = 0
+for /F "tokens=* USEBACKQ" %%F in (`ffprobe -v error -show_entries stream^=codec_type -of default^=noprint_wrappers^=1:nokey^=1 "%~1"`) DO (
+    if "%%F"=="audio" (
+        ffmpeg -i "%~1" -map 0:!count! -c:a copy "%~dnp1-Track!count!.wav"
+    )
+    set /a count += 1
+)
+pause
