61263bb5ece30f0c7f689e1a8b4d6fd408600b8f
Adam Taylor <eposvox@gmail.com>
2019-07-12 02:32:05 -0400

    Updating some potentially not working scripts.

--- OBSmcWAVextract.bat
+++ OBSmcWAVextract.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set /a count = 0
-for /F "tokens=* USEBACKQ" %%F in (`ffprobe -v error -show_entries stream^=codec_type -of default^=noprint_wrappers^=1:nokey^=1 "%~1"`) DO (
-    if "%%F"=="audio" (
-        ffmpeg -i "%~1" -map 0:!count! -c:a copy "%~dnp1-Track!count!.wav"
-    )
-    set /a count += 1
+for /R %%A in (*.mp4, *.avi, *.mov, *.wmv, *.ts, *.m2ts, *.mkv, *.mts) do (
+    echo Processing %%A
+
+    ffmpeg -strict 2 -hwaccel auto -i "%%A" -c:v hevc_nvenc -rc constqp -qp 24 -b:v 0K -c:a aac -map 0 "%%~dnpA_CRF24_HEVC.mp4"
+    echo Processed %%A
 )
+
 pause
